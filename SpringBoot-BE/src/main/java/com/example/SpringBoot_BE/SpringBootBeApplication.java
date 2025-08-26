4bd43fd455436d38428a5ee8cd4c5ee0b8b3336d
nguyendoanhung <144453577+NguyenDoanHung1011@users.noreply.github.com>
2025-08-26 14:06:36 +0700

    up

---
+++ SpringBoot-BE/src/main/java/com/example/SpringBoot_BE/SpringBootBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeApplication.class, args);
+	}
+
+}
